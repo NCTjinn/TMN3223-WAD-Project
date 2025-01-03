9f425ce195abc3506cd86fa62b3f397b7a566a38
NCTjinn <nctjinn@gmail.com>
2025-01-04 06:11:39 +0800

    removed ability to close modal after successful registration

--- public/publicRegistration.js
+++ public/publicRegistration.js
@@ -92,22 +92,4 @@ document.addEventListener('DOMContentLoaded', function() {
             alert(error.message);
         }
     });
-
-    // Get the modal
-    const modal = document.getElementById("successModal");
-
-    // Get the <span> element that closes the modal
-    const span = document.getElementsByClassName("close-button")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
 });
