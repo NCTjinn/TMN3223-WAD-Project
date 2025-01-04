413a8a9252089609824d8689a8095f5324b9f233
dulcelina9 <dulcelinapayong@gmail.com>
2025-01-04 17:20:42 +0800

    Update publicCheckout.js

--- public/publicCheckout.js
+++ public/publicCheckout.js
@@ -1,9 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Initialize cart display
-    cart.updateCartDisplay();
-
     document.getElementById('checkoutButton').addEventListener('click', function() {
         window.location.href = 'publicLogin.html';
     });
-    
 });
